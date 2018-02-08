a225286ae9e540a9af5df72b8d202fb316cbe912
jonathan poelen <jonathan.poelen@gmail.com>
2018-02-08 02:30:33 +0100

    output/{comp}-glibcxx_debug_broken_abi. file with glibcxx_debug=allow_broken_abi

--- generate.sh
+++ generate.sh
@@ -21,9 +21,11 @@ done
 
 gen compiler | while read comp ; do
   gen compiler $comp warnings pedantic > output/$comp-warnings
-  gen compiler $comp warnings=strict pedantic > output/$comp-warnings-strict
+  gen compiler $comp warnings=strict pedantic > output/$comp-warnings_strict
+  gen compiler $comp glibcxx_debug=allow_broken_abi > output/$comp-glibcxx_debug_broken_abi
   for g in suggest glibcxx_debug debug sanitizers ; do
     gen compiler $comp $g > output/$comp-$g
   done
-  cat output/$comp-glibcxx_debug output/$comp-debug output/$comp-sanitizers > output/$comp-debug-full
+  cat output/$comp-glibcxx_debug            output/$comp-debug output/$comp-sanitizers > output/$comp-debug_full
+  cat output/$comp-glibcxx_debug_broken_abi output/$comp-debug output/$comp-sanitizers > output/$comp-debug_full_broken_abi
 done
