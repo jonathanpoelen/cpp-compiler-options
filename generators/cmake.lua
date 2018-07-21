b77242f7b0b1864f01e0e45e4f6731ac18072d74
Jonathan Poelen <jonathan.poelen@gmail.com>
2018-07-21 19:37:24 +0200

    fix cmake generator

--- generators/cmake.lua
+++ generators/cmake.lua
@@ -1,38 +1,55 @@
-function tocmakeoption(optname)
-  return 'JLN_FLAG_' .. optname:upper()
-end
-
 return {
   --ignore={
   --  optimize=true,
   --  debug=true,
   --},
 
-  start=function(_, optprefix)
-    optprefix = optprefix or ''
-    _:_vcond_init({_not='NOT', else_of_else_if='else'})
-    for optname,v in pairs(_._opts) do
-      local opt = tocmakeoption(optname)
-      _:print('option(' .. opt .. ' "' .. optprefix .. string.gsub(v[3] or optname, '_', '-') ..  '" "' .. v[1] .. '")')
-      _:print('string(TOLOWER ${' .. opt .. '} ' .. opt .. ')')
-      _:print('if not ( "' .. v[2]:gsub(' ', '" STREQUAL ${' .. opt .. '} OR "') .. ' STREQUAL ${' .. opt .. '} )')
-      _:print('  error("Unknow value for ' .. opt .. ', expected ' .. v[2]:gsub(' ', ', ') .. '")')
-      _:print('endif()\n')
-    end
-    _:print('set(JLN_CXX_FLAGS "")\n')
+  tocmakeoption=function(_, optname)
+    return _.optprefix .. optname:upper()
   end,
 
-  _vcond_lvl=function(_, lvl, optname) return tocmakeoption(optname) .. ' STREQUAL "' .. lvl .. '"' end,
+  start=function(_, optprefix)
+    optprefix = optprefix:upper():gsub('-', '_') or ''
+    _.optprefix = optprefix
+    _:_vcond_init({
+      _not='NOT',
+      _and='AND',
+      _or='OR',
+      openblock='',
+      openblock='',
+      closeblock='',
+      else_of_else_if='else',
+      _else='else()',
+      endif='endif()',
+    })
+
+    for optname,v in pairs(_._opts) do
+      local opt = _:tocmakeoption(optname)
+      _:print('set(' .. opt .. ' "' .. v[1] .. '" CACHE STRING "")')
+      _:print('if(NOT(("' .. v[2]:gsub(' ', '" STREQUAL ' .. opt .. ') OR ("') .. '" STREQUAL ' .. opt .. ')))')
+      _:print('  message(FATAL_ERROR "Unknow value \\\"${' .. opt .. '}\\\" for ' .. opt .. ', expected: ' .. v[2]:gsub(' ', ', ') .. '")')
+      _:print('endif()\n')
+      _:print('if(NOT("${JLN_HELP}" STREQUAL "") AND NOT("${JLN_HELP}" STREQUAL "0"))')
+      _:print('  message(STATUS "' .. opt .. '=${' .. opt .. '}\t[' .. v[2]:gsub(' ', ', ') .. ']")')
+      _:print('endif()')
+    end
+    _:print('set(JLN_CXX_FLAGS "")\nset(JLN_LINK_FLAGS "")\n')
+  end,
+
+  _vcond_lvl=function(_, lvl, optname) return _:tocmakeoption(optname) .. ' STREQUAL "' .. lvl .. '"' end,
   _vcond_verless=function(_, major, minor) return 'CMAKE_CXX_COMPILER_VERSION VERSION_LESS "' .. major .. '.' .. minor .. '"' end,
   _vcond_comp=function(_, compiler) return 'CMAKE_CXX_COMPILER_ID MATCHES ' .. (compiler == 'gcc' and '"GNU"' or '"Clang"') end,
 
-  cxx=function(_, x) return _.indent .. '  ' .. x .. '\n' end,
-  link=function(_, x) return _.indent .. '  ' .. x .. '\n' end,
-  define=function(_, x) return _.indent .. '  -D' .. x .. '\n' end,
+  cxx=function(_, x) return ' ' .. x end,
+  link=function(_, x) return ' ' .. x end,
+  define=function(_, x) return ' -D' .. x end,
 
   _vcond_toflags=function(_, cxx, links, defines)
-    return ((#cxx ~= 0 or #defines ~= 0) and _.indent .. '  add_definitions(\n' .. cxx .. defines .. _.indent .. '  )' or '')
-        .. ((#cxx ~= 0 or #defines ~= 0) and #links ~= 0 and '\n' or '')
-        .. (#links ~= 0                  and _.indent .. '  link_libraries(\n' .. links           .. _.indent .. '  )' or '')
+    return ((#cxx ~= 0 or #defines ~= 0) and _.indent .. '  string(CONCAT JLN_CXX_FLAGS ${JLN_CXX_FLAGS} "' .. cxx .. defines .. '")\n' or '')
+        .. (#links ~= 0 and _.indent .. '  string(CONCAT JLN_LINK_FLAGS ${JLN_LINK_FLAGS} "' .. links .. '")' or '')
+  end,
+
+  stop=function(_)
+    return _:get_output() .. 'string(STRIP "${JLN_CXX_FLAGS}" JLN_CXX_FLAGS)\nstring(STRIP "${JLN_LINK_FLAGS}" JLN_LINK_FLAGS)'
   end
 }
--- test/test.sh
+++ test/test.sh
@@ -2,29 +2,45 @@
 
 test_success()
 {
-  echo -n "test_success: $@"
+  echo -n "test_success: $@  "
   eval "$@ >/dev/null" || {
-    echo -e "  \e[31mfailure\e[0m"
+    echo -e "\e[31mfailure\e[0m"
     exit 1
   }
-  echo -e "  \e[32mok\e[0m"
+  echo -e "\e[32mok\e[0m"
 }
 
 test_failure()
 {
-  echo -n "test_failure: $@"
+  echo -n "test_failure: $@  "
   eval "$@ >/dev/null" && {
-    echo -e "  \e[31mfailure\e[0m"
+    echo -e "\e[31mfailure\e[0m"
     exit 1
   }
-  echo -e "  \e[32mok\e[0m"
+  echo -e "\e[32mok\e[0m"
 }
 
+cd "$(dirname "$0")"
+d=$PWD
 
-cd premake5
-test_success 'premake5 | grep Wall'
-test_failure 'premake5 | grep Weverything'
-test_success 'premake5 --cc=gcc | grep Wall'
-test_success 'premake5 --cc=clang | grep Weverything'
-test_failure 'premake5 | grep lto'
-test_success 'premake5 --jln-lto=on | grep lto'
+if [ -z "$1" ] || [ "$1" = premake5 ]; then
+  cd premake5
+  test_success 'premake5 | grep Wall'
+  test_failure 'premake5 | grep Weverything'
+  test_success 'premake5 --cc=gcc | grep Wall'
+  test_success 'premake5 --cc=clang | grep Weverything'
+  test_failure 'premake5 | grep lto'
+  test_success 'premake5 --jln-lto=on | grep lto'
+fi
+
+if [ -z "$1" ] || [ "$1" = cmake ]; then
+  mkdir -p /tmp/compgentest
+  cd /tmp/compgentest
+  rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake Makefile test
+  test_success "cmake '$d/cmake' | grep Wall"
+  test_failure "cmake '$d/cmake' | grep Weverything"
+  test_success "cmake '$d/cmake' -DCMAKE_CXX_COMPILER=gcc | grep Wall"
+  test_success "cmake '$d/cmake' -DCMAKE_CXX_COMPILER=clang++ | grep Weverything"
+  test_failure "cmake '$d/cmake' | grep lto"
+  test_success "cmake '$d/cmake' -DJLN_LTO=on | grep lto"
+fi
