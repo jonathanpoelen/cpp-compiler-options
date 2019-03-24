722ffb749b3d91445c10149677887d75b903b505
Jonathan Poelen <jonathan.poelen@gmail.com>
2019-03-24 01:47:44 +0100

    update_readme: update list of not default values

--- update_readme.lua
+++ update_readme.lua
@@ -38,8 +38,34 @@ while true do
 end
 
 local options = io.popen('./compiler-options.lua generators/options.lua', 'r'):read('a')
+local notdefaults = {}
+for opt,v in options:gmatch('(%w+) = (%w+)') do
+  if v ~= 'default' then
+    local t = notdefaults[v]
+    if not t then
+      t = {}
+      notdefaults[v] = t
+    end
+    t[#t+1] = opt
+  end
+end
+local strnotdefault = ''
+for k,l in pairs(notdefaults) do
+  local stropt
+  if #l == 1 then
+    stropt = l[1]
+  else
+    local last = table.remove(l)
+    stropt = table.concat(l, '`, `') .. '` and `' .. last
+  end
+
+  strnotdefault = strnotdefault
+  .. (#strnotdefault == 0 and ' `' or ' ; `')
+  .. stropt
+  .. (#l == 1 and '` is `' or '` are `') .. k .. '`'
+end
 contents = contents
 :gsub('(<!%-%- summary %-%->)\n.+(\n<!%-%- /summary %-%->)', '%1' .. summary:gsub('%%%%', '%%') .. '%2')
-:gsub('(<!%-%- %./compiler%-options%.lua generators/options%.lua color %-%->\n```ini\n).+(```\n<!%-%- %./compiler%-options%.lua %-%->)', '%1' .. options .. '%2')
+:gsub('(<!%-%- %./compiler%-options%.lua generators/options%.lua color %-%->\n```ini\n).+(```\n<!%-%- %./compiler%-options%.lua %-%->)\n\n.-\n\n.-\n', '%1' .. options .. '%2\n\nThe value `default` does nothing.\n\nIf not specified,' .. strnotdefault .. '.\n')
 
 io.open(README, 'w'):write(contents)
