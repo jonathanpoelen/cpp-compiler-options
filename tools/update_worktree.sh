97d0f17669218d59b8ffe2f070da720a7d7b4ca9
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-05-17 19:48:09 +0200

    [tool] update_worktree.sh: commit output/ directory from master

--- tools/update_worktree.sh
+++ tools/update_worktree.sh
@@ -9,6 +9,8 @@ cd $(realpath $(dirname "$0"))/..
 
 ${LUA_BIN:-lua} ./tools/update_worktree.lua
 
+git add output && git commit -m '[output] update' ||:
+
 for d in bjam cmake meson premake5 scons ; do
   echo "> $d"
   cd w/$d
