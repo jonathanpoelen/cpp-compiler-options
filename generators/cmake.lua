831864fb46d5d6c863df2dd11bc01b50819aca29
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-11-29 23:50:49 +0100

    [cmake] fix msvc_isystem=external_as_include_system_flag

--- generators/cmake.lua
+++ generators/cmake.lua
@@ -299,9 +299,9 @@ endif()
       for k,d in pairs(datas) do
         if k == 'cxx' then
           cxx_flags = _.indent .. 'set(CMAKE_' .. cat .. '_FLAGS "${CMAKE_' .. cat .. '_FLAGS} '
-                      .. table.concat(d, ' ') .. ' " PARENT_SCOPE)'
+                      .. table.concat(d, ' ') .. ' " CACHE INTERNAL "")'
         elseif k == 'SYSTEM_FLAG' then
-          isystem_flag = _.indent .. 'set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "' .. d .. ' " PARENT_SCOPE)'
+          isystem_flag = _.indent .. 'set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "' .. d .. ' " CACHE INTERNAL "")'
         else
           return 'Unknow ' .. k
         end
