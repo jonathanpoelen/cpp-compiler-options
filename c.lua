ff0cb9f5f4105ebd98704bab95d892c48fa8fb5b
Jonathan Poelen <jpoelen@wallix.com>
2024-06-24 15:26:11 +0200

    update premake5 files

--- c.lua
+++ c.lua
@@ -784,18 +784,29 @@ function jln_c_getoptions(values, disable_others, print_compiler)
       end
     else
       if values['switch_warnings'] ~= 'default' then
-        if ( values['switch_warnings'] == 'on' or values['switch_warnings'] == 'mandatory_default' ) then
+        if values['switch_warnings'] == 'on' then
           table_insert(jln_buildoptions, "-Wswitch")
-          table_insert(jln_buildoptions, "-Wswitch-default")
+          table_insert(jln_buildoptions, "-Wno-switch-default")
         else
-          if ( values['switch_warnings'] == 'exhaustive_enum' or values['switch_warnings'] == 'exhaustive_enum_and_mandatory_default' ) then
+          if values['switch_warnings'] == 'mandatory_default' then
             table_insert(jln_buildoptions, "-Wswitch")
-            table_insert(jln_buildoptions, "-Wswitch-enum")
-            table_insert(jln_buildoptions, "-Wno-switch-default")
+            table_insert(jln_buildoptions, "-Wswitch-default")
           else
-            table_insert(jln_buildoptions, "-Wno-switch")
-            table_insert(jln_buildoptions, "-Wno-switch-enum")
-            table_insert(jln_buildoptions, "-Wno-switch-default")
+            if values['switch_warnings'] == 'exhaustive_enum' then
+              table_insert(jln_buildoptions, "-Wswitch")
+              table_insert(jln_buildoptions, "-Wswitch-enum")
+              table_insert(jln_buildoptions, "-Wno-switch-default")
+            else
+              if values['switch_warnings'] == 'exhaustive_enum_and_mandatory_default' then
+                table_insert(jln_buildoptions, "-Wswitch")
+                table_insert(jln_buildoptions, "-Wswitch-enum")
+                table_insert(jln_buildoptions, "-Wswitch-default")
+              else
+                table_insert(jln_buildoptions, "-Wno-switch")
+                table_insert(jln_buildoptions, "-Wno-switch-enum")
+                table_insert(jln_buildoptions, "-Wno-switch-default")
+              end
+            end
           end
         end
       end
