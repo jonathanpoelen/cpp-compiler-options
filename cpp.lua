d0dca2f8f1caa3c10ef8db878121dabb67e16c1d
Jonathan Poelen <jonathan.poelen@gmail.com>
2024-11-20 00:53:14 +0100

    update premake5 files

--- cpp.lua
+++ cpp.lua
@@ -1,11 +1,11 @@
 --  ```lua
 --  -- launch example: premake5 --jln-sanitizers=on
---  
+--
 --  include "cpp.lua"
---  
+--
 --  -- Registers new command-line options and set default values
 --  jln_newoptions({warnings='very_strict'})
---  
+--
 --      -- getoptions(values = {}, disable_others = false, print_compiler = false)
 --      -- `values`: table. ex: {warnings='on'}
 --      -- `values` can have 3 additional fields:
@@ -19,14 +19,14 @@
 --      local options = flags.getoptions({elide_type='on'})
 --      for _,opt in ipairs(options.cxxflags) do target:add('cxxflags', opt, {force=true}) end
 --      for _,opt in ipairs(options.ldflags) do target:add('ldflags', opt, {force=true}) end
---  
+--
 --      -- or equivalent (return also options)
 --      flags.setoptions(target, {elide_type='on'})
---  
+--
 --      -- return the merge of the default values and new value table
 --      local values = flags.tovalues({elide_type='on'}, --[[disable_others:bool]])
 --      print(values)
---  
+--
 --  -- jln_getoptions(values = {}, disable_others = false, print_compiler = false)
 --  -- `values`: table. ex: {warnings='on'}
 --  -- `values` can have 3 additional fields:
@@ -40,26 +40,26 @@
 --  local mylib_options = jln_getoptions({elide_type='on'})
 --  buildoptions(mylib_options.buildoptions)
 --  linkoptions(mylib_options.linkoptions)
---  
+--
 --  -- or equivalent
 --  jln_setoptions({elide_type='on'})
---  
+--
 --  -- returns the merge of the default values and new value table
 --  local values = jln_tovalues({elide_type='on'}, --[[disable_others:bool]])
 --  print(values)
---  
+--
 --  -- NOTE: for C, jln_ prefix function becomes jln_c_
 --  ```
---  
---  
+--
+--
 --  # Options
---  
+--
 --  Supported options are (alphabetically in a category):
---  
+--
 --  <!-- ./compiler-options.lua generators/list_options.lua --color -->
 --  ```ini
 --  # Warning:
---  
+--
 --  analyzer = default off on taint
 --  analyzer_too_complex_warning = default off on
 --  analyzer_verbosity = default 0 1 2 3
@@ -76,15 +76,15 @@
 --  warnings = on default off strict very_strict
 --  warnings_as_error = default off on basic
 --  windows_abi_compatibility_warnings = off default on
---  
+--
 --  # Pedantic:
---  
+--
 --  msvc_conformance = all default all_without_throwing_new
 --  pedantic = on default off as_error
 --  stl_fix = on default off
---  
+--
 --  # Debug:
---  
+--
 --  debug = default off on line_tables_only gdb lldb sce
 --  float_sanitizers = default off on
 --  integer_sanitizers = default off on
@@ -93,28 +93,28 @@
 --  sanitizers = default off on
 --  stl_debug = default off on allow_broken_abi allow_broken_abi_and_bugs assert_as_exception
 --  var_init = default uninitialized pattern zero
---  
+--
 --  # Optimization:
---  
+--
 --  cpu = default generic native
 --  linker = default bfd gold lld native
 --  lto = default off on normal fat thin
 --  optimization = default 0 g 1 2 3 fast size z
 --  whole_program = default off on strip_all
---  
+--
 --  # C++:
---  
+--
 --  exceptions = default off on
 --  rtti = default off on
---  
+--
 --  # Hardening:
---  
+--
 --  control_flow = default off on branch return allow_bugs
 --  relro = default off on full
 --  stack_protector = default off on strong all
---  
+--
 --  # Other:
---  
+--
 --  color = default auto never always
 --  coverage = default off on
 --  diagnostics_format = default fixits patch print_source_range_info
@@ -127,11 +127,11 @@
 --  windows_bigobj = on default
 --  ```
 --  <!-- ./compiler-options.lua -->
---  
+--
 --  The value `default` does nothing.
---  
+--
 --  If not specified:
---  
+--
 --  - `msvc_conformance` is `all`
 --  - `msvc_diagnostics_format` is `caret`
 --  - `ndebug` is `with_optimization_1_or_above`
@@ -148,25 +148,25 @@
 --    - `switch_warnings`
 --    - `warnings`
 --    - `windows_bigobj`
---  
+--
 --  <!-- enddefault -->
---  
+--
 --  - `control_flow=allow_bugs`
 --    - clang: Can crash programs with "illegal hardware instruction" on totally unlikely lines. It can also cause link errors and force `-fvisibility=hidden` and `-flto`.
 --  - `stl_debug=allow_broken_abi_and_bugs`
 --    - clang: libc++ can crash on dynamic memory releases in the standard classes. This bug is fixed with the library associated with version 8.
 --  - `msvc_isystem=external_as_include_system_flag` is only available with `cmake`.
---  
---  
+--
+--
 --  ## Recommended options
---  
+--
 --  category | options
 --  ---------|---------
 --  debug | `control_flow=on`<br>`debug=on`<br>`sanitizers=on`<br>`stl_debug=allow_broken_abi` or `on`<br>
 --  release | `cpu=native`<br>`linker=gold`, `lld` or `native`<br>`lto=on` or `thin`<br>`optimization=3`<br>`rtti=off`<br>`whole_program=strip_all`
 --  security | `control_flow=on`<br>`relro=full`<br>`stack_protector=strong`<br>`pie=PIE`
 --  really strict warnings | `pedantic=as_error`<br>`shadow_warnings=local`<br>`suggestions=on`<br>`warnings=very_strict`
---  
+--
 --  
 
 -- File generated with https://github.com/jonathanpoelen/cpp-compiler-options
