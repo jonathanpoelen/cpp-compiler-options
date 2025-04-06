c5375f02433c158f22dbec79e13bbcb68d09013c
Jonathan Poelen <jonathan.poelen@gmail.com>
2025-04-06 14:33:18 +0200

    [generator] list_options: add value description with --verbose and support options in several categories

--- generators/list_options.lua
+++ generators/list_options.lua
@@ -1,9 +1,28 @@
-local knwon_opts = {}
+local known_opts = {}
 local errors = {}
+
+local _color_map = {
+  '\027[34m',
+  '\027[35m',
+  -- specific color for certain values
+  on='\027[32m',
+  off='\027[31m',
+  default='\027[37m',
+}
+local color_size = 2
+local _color_reset = '\027[0m'
+local _color_reverse = ';7m'
+local _color_sep = '\027[37m'
+local _color_desc = '\027[37m'
+local _color_cat = '\027[1m'
 
 local table_insert = table.insert
 
-local without_space_or_error = function(self, s)
+local function add_opt(option)
+  known_opts[option.name] = {option.kvalues}
+end
+
+local without_space_otherwise_error = function(self, s)
   if s:find(' ') then
     table_insert(errors, '"' .. s .. '" contains a space')
   end
@@ -15,132 +34,195 @@ end
 
 return {
   start=function(self, ...)
-    local show_profile, color, categorized, verbose
-    local help = function()
-      print(self.generator_name .. ' [-h] [-v] [--categorized] [--profile] [--color]')
-      return false
-    end
+    -- cli options
+    local show_profile, colorize, categorized, verbose
+
+    -- cli and parse
+    -- @{
     local cli = {
       ['--categorized']=function() categorized=true end,
       ['--profile']=function() show_profile=true end,
-      ['--color']=function() color=true end,
+      ['--color']=function() colorize=true end,
+      ['--verbose']=function() verbose=true end,
       ['-v']=function() verbose=true end,
-      ['-h']=help,
-      ['--help']=help,
     }
+
     for _,v in ipairs({...}) do
       local opt = cli[v]
       if not opt then
-        return help()
-      end
-      if opt() == false then
+        local f = (v == '-h' or v == '--help') and io.stdout or io.stderr
+        f:write(self.generator_name .. ' [{-h | --help] [{-v | --verbose}] [--categorized] [--profile] [--color]\n')
+        f:flush()
         return false
       end
+      opt()
+    end
+    -- @}
+
+    -- reset table, populated with add_opt()
+    known_opts = {}
+
+    local color_map = colorize and _color_map or ''
+    local color_reset = colorize and _color_reset or ''
+    local color_reverse = colorize and _color_reverse or ''
+    local color_sep = colorize and _color_sep or ''
+    local color_desc = colorize and _color_desc or ''
+
+    local string_parts = {}
+    -- {opt_name: string = [string, used: bool]}
+    local stringify_options = {}
+
+    -- init option
+
+    local icolor, desc
+    local color = ''
+    for option in self:getoptions() do
+      add_opt(option)
+
+      -- option name
+
+      table_insert(string_parts, option.name)
+      if not verbose then
+        table_insert(string_parts, ' ')
+        table_insert(string_parts, color_sep)
+        table_insert(string_parts, '=')
+      else
+        table_insert(string_parts, '\n')
+      end
+
+      -- option description
+
+      if verbose and option.description then
+        table_insert(string_parts, '  ')
+        table_insert(string_parts, color_desc)
+        table_insert(string_parts, option.description)
+        table_insert(string_parts, color_reset)
+        table_insert(string_parts, '\n')
+      end
+
+      -- option values
+
+      icolor = 0
+      for i,opt_value in ipairs(option.ordered_values) do
+        if colorize then
+          color = color_map[x]
+          if not color then
+            color = color_map[icolor % color_size + 1]
+            icolor = icolor + 1
+          end
+        end
+
+        -- value seperator
+
+        if verbose then
+          desc = option.value_descriptions[i]
+          table_insert(string_parts, color_desc)
+          table_insert(string_parts, '  - ')
+        else
+          table_insert(string_parts, ' ')
+        end
+
+        -- value name
+
+        if i == 1 then
+          table_insert(string_parts, color:sub(0,-2))
+          table_insert(string_parts, color_reverse)
+          table_insert(string_parts, opt_value)
+          table_insert(string_parts, color_reset)
+        else
+          table_insert(string_parts, color)
+          table_insert(string_parts, opt_value)
+        end
+
+        -- value description
+
+        if desc then
+          table_insert(string_parts, ': ')
+          table_insert(string_parts, color_desc)
+          table_insert(string_parts, desc)
+        end
+        if verbose then
+          table_insert(string_parts, '\n')
+        end
+      end
+
+      -- option seperation
+
+      if not verbose then
+        table_insert(string_parts, '\n')
+      end
+      table_insert(string_parts, color_reset)
+
+      if categorized then
+        stringify_options[option.name] = {table.concat(string_parts)}
+        string_parts = {}
+      end
     end
 
-    local push_opt_for_print, opt_for_print_end
+    -- init categories
+
     if categorized then
-      local categorized_opts = {}
+      local color_cat = colorize and _color_cat or ''
+      local opt
+
+      local push_title = function(name)
+        table_insert(string_parts, color_cat)
+        table_insert(string_parts, '# ')
+        table_insert(string_parts, name)
+        table_insert(string_parts, color_reset)
+        table_insert(string_parts, ':\n\n')
+      end
+
+      -- populate categories
+
+      string_parts = {}
       for _,infos in ipairs(self._opts_by_category) do
-        local i = #categorized_opts + 1
-        categorized_opts[i] = {infos[1], {}}
+        push_title(infos[1])
+        table.sort(infos[2])
         for _,optname in ipairs(infos[2]) do
-          categorized_opts[optname] = i
+          opt = stringify_options[optname]
+          opt[2] = true
+          table_insert(string_parts, opt[1])
         end
-      end
-      local other_cat = #categorized_opts + 1
-      categorized_opts[other_cat] = {'Other', {}}
-
-      push_opt_for_print = function(option, str, desc)
-        local strings = categorized_opts[categorized_opts[option.name] or other_cat][2]
-        table_insert(strings, str)
-        if verbose and desc then
-          table_insert(strings, '  ' .. desc)
-        end
+        table_insert(string_parts, '\n')
       end
 
-      opt_for_print_end = function()
-        local strings = {}
-        local first = true
-        for _,infos in ipairs(categorized_opts) do
-          if #infos[2] ~= 0 then
-            if not first then
-              table_insert(strings, '')
-            end
-            table_insert(strings, color and ('\027[1m# ' .. infos[1] .. '\027[0m:\n')
-                                         or ('# ' .. infos[1] .. ':\n'))
-            first = false
-            for _,str in ipairs(infos[2]) do
-              table_insert(strings, str)
-            end
-          end
-        end
-        print(table.concat(strings, '\n'))
-      end
-    else
-      push_opt_for_print = function(option, str, desc)
-        print(str)
-        if verbose and desc then
-          print('  ' .. desc)
-        end
-      end
-      opt_for_print_end = function() end
-    end
+      -- populate "Other" category
 
-    local add_opt = function(option)
-      knwon_opts[option.name] = {option.kvalues}
-    end
-
-    if color then
-      local color_map = {
-        on='\027[32m',
-        off='\027[31m',
-        default='\027[37m',
-      }
-      color_map[0] = '\027[34m'
-      color_map[1] = '\027[35m'
-      local color_size = 2
-      for option in self:getoptions() do
-        local str, ic = option.name .. ' \027[37m=', 0
-        for i,x in ipairs(option.ordered_values) do
-          local c = color_map[x]
-          if not c then
-            c = color_map[ic % color_size]
-            ic = ic + 1
-          end
-          str = str .. ' ' .. (i == 1
-            and (c:sub(0,-2) .. ';7m' .. x .. '\027[0m')
-            or (c .. x))
+      push_title('Other')
+      for option in self:getoptions() do -- getoptions() is always sorted
+        opt = stringify_options[option.name]
+        if not opt[2] then
+          table_insert(string_parts, opt[1])
         end
-        push_opt_for_print(option, str .. '\027[0m',
-                           option.description and ('\027[37m' .. option.description .. '\027[0m'))
-        add_opt(option)
-      end
-    else
-      for option in self:getoptions() do
-        push_opt_for_print(option, option.name .. ' = '
-                           .. table.concat(option.ordered_values, ' '),
-                           option.description)
-        add_opt(option)
       end
     end
 
-    opt_for_print_end()
+    -- populate profile
 
     if show_profile then
-      print('\n\nProfiles:')
-      table.sort(self._opts_build_type)
+      table_insert(string_parts, '\n\nProfiles:\n')
       for name, opts in self:getbuildtype() do
-        print('\n' .. name)
+        table_insert(string_parts, '\n' .. name)
         for _,xs in ipairs(opts) do
-          print(' - ' .. xs[1] .. ' = ' .. xs[2])
+          table_insert(string_parts, '\n - ' .. xs[1] .. ' = ' .. xs[2])
         end
       end
+      table_insert(string_parts, '\n')
     end
+
+    -- print
+
+    if show_profile then
+      io.stdout:write('Options:\n\n')
+    end
+
+    io.stdout:write(table.concat(string_parts))
+    io.stdout:flush()
   end,
 
   resetopt=function(self, optname)
-    local known = knwon_opts[optname]
+    local known = known_opts[optname]
     if not known then
       if is_available(self, optname) then
         table_insert(errors, '_koptions[' .. optname .. ']: unknown key')
@@ -149,7 +231,7 @@ return {
   end,
 
   startoptcond=function(self, optname)
-    local known = knwon_opts[optname]
+    local known = known_opts[optname]
     if not known then
       if is_available(self, optname) then
         table_insert(errors, '_koptions[' .. optname .. ']: unknown key')
@@ -161,7 +243,7 @@ return {
 
   startcond=function(self, x, optname)
     if x.lvl then
-      local known = knwon_opts[optname]
+      local known = known_opts[optname]
       if not known then
         if is_available(self, optname) then
           table_insert(errors, '_koptions[' .. optname .. ']: unknown key')
@@ -184,7 +266,7 @@ return {
   end,
 
   stop=function()
-    for k,opts in pairs(knwon_opts) do
+    for k,opts in pairs(known_opts) do
       if not opts[2] then
         table_insert(errors, '_koptions[' .. k .. ']: not used in the tree')
       end
@@ -194,7 +276,7 @@ return {
     end
   end,
 
-  cxx=without_space_or_error,
-  link=without_space_or_error,
+  cxx=without_space_otherwise_error,
+  link=without_space_otherwise_error,
   act=function() return true end,
 }
